dd875f53af9b8626109b7ce76102023cb5808e0a
mannu <mannu.singh@habilelabs.io>
2022-08-17 09:59:36 +0530

    feat: added about module in lazy loading concept

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ const routes: Routes = [
   { path: '', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule), canActivate: [AuthGuard] },
   { path: 'home', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule), canActivate: [AuthGuard] },
   { path: 'error', loadChildren: () => import('./modules/error/error.module').then(m => m.ErrorModule), canActivate: [AuthGuard] },
+  { path: 'about', loadChildren: () => import('./modules/about/about.module').then(m => m.AboutModule), canActivate: [AuthGuard]},
   { path: '**', loadChildren: () => import('./modules/error/error.module').then(m => m.ErrorModule), canActivate: [AuthGuard] },
 
 
